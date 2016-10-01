b491345c2e713227cfc1f786f9ed622ba90cec2c
Ben Attenborough <battenborough@gmail.com>
2016-10-01 22:12:47 +0100

    Work on charts

--- js/application.js
+++ js/application.js
@@ -9,3 +9,47 @@ dismissButton.addEventListener('click', dismissAlert);
 function dismissAlert() {
     dismissButton.parentNode.style.display = 'none';
 }
+
+var ctx = document.getElementById("myChart");
+
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fill: false,
+            lineTension: 0,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+            spanGaps: false,
+        }
+    ]
+};
+
+var options = {
+    scales: {
+        yAxes: [{
+        }]
+    },
+    responsive: true
+};
+
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+});
