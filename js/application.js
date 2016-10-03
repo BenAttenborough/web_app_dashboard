ccb30058ac04bc860d4372e978bea24b4befdc79
Ben Attenborough <battenborough@gmail.com>
2016-10-03 13:08:50 +0100

    Hmm, doughnut

--- js/application.js
+++ js/application.js
@@ -10,6 +10,22 @@ function dismissAlert() {
     dismissButton.parentNode.style.display = 'none';
 }
 
+var colorLight = {
+    rgb: '116,119,191',
+    hex: '#7477BF'
+};
+
+var colorMed = {
+    rgb: '129,201,143',
+    hex: '#81C98F'
+};
+
+
+var colorDark = {
+    rgb: '77,76,114',
+    hex: '#4D4C72'
+};
+
 var visitorsChart = {
     ctx: document.getElementById("chart-visitors"),
     type: 'line',
@@ -20,18 +36,18 @@ var visitorsChart = {
                 label: "Traffic",
                 fill: true,
                 lineTension: 0,
-                backgroundColor: "rgba(116,119,191,0.4)",
-                borderColor: "rgba(115,119,191,1)",
+                backgroundColor: "rgba(" + colorLight.rgb + ",0.4)",
+                borderColor: "rgba(" + colorLight.rgb + ",1)",
                 borderCapStyle: 'butt',
                 borderDash: [],
                 borderDashOffset: 0.0,
                 borderJoinStyle: 'miter',
-                pointBorderColor: "rgba(115,119,191,1)",
+                pointBorderColor: "rgba(" + colorLight.rgb + ",1)",
                 pointBackgroundColor: "#fff",
                 pointBorderWidth: 1,
                 pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(116,119,191,1)",
-                pointHoverBorderColor: "rgba(115,119,191,1)",
+                pointHoverBackgroundColor: "rgba(" + colorLight.rgb + ",1)",
+                pointHoverBorderColor: "rgba(" + colorLight.rgb + ",1)",
                 pointHoverBorderWidth: 2,
                 pointRadius: 5,
                 pointHitRadius: 10,
@@ -62,7 +78,7 @@ var trafficChart = {
         datasets: [
             {
                 label: "Daily Traffic",
-                backgroundColor: "rgba(116,119,191,1)",
+                backgroundColor: "rgba(" + colorLight.rgb + ",1)",
                 data: [75, 100, 275, 125, 225, 200, 100],
             }
         ]
@@ -84,4 +100,34 @@ var myTrafficChart = new Chart(trafficChart.ctx, {
     type: trafficChart.type,
     data: trafficChart.data,
     options: trafficChart.options
+});
+
+var mobileUsersChart = {
+    ctx: document.getElementById("chart-mobile"),
+    type: 'doughnut',
+    data: {
+        labels: ["Phones", "Tablets", "Descktop"],
+        datasets: [
+            {
+                label: "Mobile Users",
+                backgroundColor: "rgba(116,119,191,1)",
+                data: [15, 10, 75],
+                backgroundColor: [
+                    colorLight.hex,
+                    colorMed.hex,
+                    colorDark.hex
+                ]
+            }
+        ]
+    },
+
+    options: {
+        responsive: true
+    }
+};
+
+var myMobileChart = new Chart(mobileUsersChart.ctx, {
+    type: mobileUsersChart.type,
+    data: mobileUsersChart.data,
+    options: mobileUsersChart.options
 });
