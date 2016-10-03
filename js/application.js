b29298040aa7caa8cc3975aa138d36ac9383e3fb
Ben Attenborough <battenborough@gmail.com>
2016-10-03 12:42:57 +0100

    Added bar chart

--- js/application.js
+++ js/application.js
@@ -10,46 +10,78 @@ function dismissAlert() {
     dismissButton.parentNode.style.display = 'none';
 }
 
-var ctx = document.getElementById("myChart");
-
-var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January"],
-    datasets: [
-        {
-            label: "Traffic",
-            fill: true,
-            lineTension: 0,
-            backgroundColor: "rgba(116,119,191,0.4)",
-            borderColor: "rgba(115,119,191,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(115,119,191,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(116,119,191,1)",
-            pointHoverBorderColor: "rgba(115,119,191,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 5,
-            pointHitRadius: 10,
-            data: [0, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
-            spanGaps: false,
-        }
-    ]
-};
-
-var options = {
-    scales: {
-        yAxes: [{
-        }]
-    },
-    responsive: true
-};
-
-var myLineChart = new Chart(ctx, {
+var visitorsChart = {
+    ctx: document.getElementById("chart-visitors"),
     type: 'line',
-    data: data,
-    options: options
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January"],
+        datasets: [
+            {
+                label: "Traffic",
+                fill: true,
+                lineTension: 0,
+                backgroundColor: "rgba(116,119,191,0.4)",
+                borderColor: "rgba(115,119,191,1)",
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: "rgba(115,119,191,1)",
+                pointBackgroundColor: "#fff",
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgba(116,119,191,1)",
+                pointHoverBorderColor: "rgba(115,119,191,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 5,
+                pointHitRadius: 10,
+                data: [0, 750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+                spanGaps: false,
+            }
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{}]
+        },
+        responsive: true
+    }
+};
+
+var myVisitorsChart = new Chart(visitorsChart.ctx, {
+    type: visitorsChart.type,
+    data: visitorsChart.data,
+    options: visitorsChart.options
+});
+
+var trafficChart = {
+    ctx: document.getElementById("chart-traffic"),
+    type: 'bar',
+    data: {
+        labels: ["S", "M", "T", "W", "T", "F", "S"],
+        datasets: [
+            {
+                label: "Daily Traffic",
+                backgroundColor: "rgba(116,119,191,1)",
+                data: [75, 100, 275, 125, 225, 200, 100],
+            }
+        ]
+    },
+
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        },
+        responsive: true
+    }
+};
+
+var myTrafficChart = new Chart(trafficChart.ctx, {
+    type: trafficChart.type,
+    data: trafficChart.data,
+    options: trafficChart.options
 });
